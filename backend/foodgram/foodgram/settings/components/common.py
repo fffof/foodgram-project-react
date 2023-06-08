872df0fb1a3385c5e0f39a2c47976476e791e0da
Антон Трифанов <fffof.a.v@yandex.ru>
2023-06-08 12:26:07 +0300

    changed

--- backend/foodgram/foodgram/settings/components/common.py
+++ backend/foodgram/foodgram/settings/components/common.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', default=True)
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', default='*').split(' ')
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
