ac197f03545628bbda7e134fd18f85c54aa6cc1e
Антон Трифанов <fffof.a.v@yandex.ru>
2023-06-09 18:47:56 +0300

    changed

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -117,7 +117,7 @@ class RecipeViewSet(viewsets.ModelViewSet, CreateDeleteMixin):
     def get_serializer_class(self):
         if self.action == "create" or (self.action == "update"):
             return RecipeSerializer
-        elif self.action == "partial_update":
+        if self.action == "partial_update":
             return RecipeSerializer
         return RecipeViewSerializer
 
